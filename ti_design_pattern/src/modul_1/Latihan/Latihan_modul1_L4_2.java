c9e5a490032e744e5b191124f7ba29621514f90b
nazrilka <ghostwithcard@gmail.com>
2026-03-02 13:49:55 +0700

    Quick Review

---
+++ ti_design_pattern/src/modul_1/Latihan/Latihan_modul1_L4_2.java
@@ -0,0 +1,16 @@
+package modul_1.Latihan;
+
+public class Latihan_modul1_L4_2 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 20) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+
+    }
+}
---
+++ ti_design_pattern/src/modul_1/Latihan/Latihan_modul_1_L4.java
@@ -0,0 +1,13 @@
+package modul_1.Latihan;
+
+public class Latihan_modul_1_L4 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 != 0) {   // cek apakah ganjil
+                System.out.println(i);
+            }
+        }
+
+    }
+}
